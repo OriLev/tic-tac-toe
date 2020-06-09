770b056cd0ed84a1c3bf9697d963f4e42b8cd056
Ori Levitan <orilevitan2@gmail.com>
2020-06-09 15:38:36 +0300

    Updates

---
+++ precache-manifest.0ea22c12c7b36782a4e2481fff9cb6cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a200c7eb14d4232461697c384be8a1d",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "a42cf8e8f61d19d8be6c",
+    "url": "/tic-tac-toe/static/css/main.38e3b346.chunk.css"
+  },
+  {
+    "revision": "4d794389f02f7f18a70f",
+    "url": "/tic-tac-toe/static/js/2.66eb06a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.66eb06a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a42cf8e8f61d19d8be6c",
+    "url": "/tic-tac-toe/static/js/main.fb3eb162.chunk.js"
+  },
+  {
+    "revision": "b83c3f5dfddd51f16016",
+    "url": "/tic-tac-toe/static/js/runtime-main.e9088102.js"
+  }
+]);
